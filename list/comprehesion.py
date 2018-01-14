6d1523282415d6374586cdf8b1ba800026fd78d8
Andy <ithaibo@sina.com>
2018-01-14 19:38:39 +0800

    modify

--- list/comprehesion.py
+++ list/comprehesion.py
@@ -1,11 +1,24 @@
 even_numbers = [x for x in range(5) if x % 2 == 0]
-print 'even numbers in range 5:'
-print even_numbers
+# print 'even numbers in range 5:'
+# print even_numbers
 
 squares = [x*x for x in range(5)]
-print 'square numbers in range 5:'
-print squares
+# print 'square numbers in range 5:'
+# print squares
 
 even_squares = [x * x for x in even_numbers]
-print 'even square numbers in even numbers:'
-print even_squares
+# print 'even square numbers in even numbers:'
+# print even_squares
+
+square_dict = {x : x * x for x in range(5)}
+# print 'square dictionary in range 5:'
+# print square_dict
+
+zeros = [0 for _ in even_numbers]
+# print zeros
+
+pairs = [(x,y)
+            for x in range(10)
+            for y in range(x+1, 10)]
+            
+print pairs
