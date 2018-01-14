52fce3001ddc4d1b11cf5f8dd462fab213dce82b
Andy <ithaibo@sina.com>
2018-01-14 21:33:44 +0800

    modify

--- file/read.py
+++ file/read.py
@@ -1,9 +1,5 @@
-# f = open('../README.md', 'r')
-# data = f.read()
-# f.closed
-
-# print(data)
-
-f = open("../README.md", 'w')
-f.write("2018-01-05 15:54")
+f = open('../README.md', 'r')
+data = f.read()
 f.closed
+
+print(data)
---
+++ file/write.py
@@ -0,0 +1,3 @@
+f = open("../README.md", 'a')
+f.write("\n2018-01-14 21:25")
+f.closed
