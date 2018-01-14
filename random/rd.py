1137c841816be8d2eced0b17c83583121c5733ed
Andy <ithaibo@sina.com>
2018-01-14 21:45:31 +0800

    modify

--- random/rd.py
+++ random/rd.py
@@ -1,3 +1,16 @@
 import random
 
-print(random.random())
+# random.seed(100)
+# print(random.random())
+
+
+# print(random.randrange(10))
+
+
+# print(random.randrange(3, 6))
+
+# 对list进行随机重排列
+up_to_ten = range(10)
+print(up_to_ten)
+random.shuffle(up_to_ten)
+print(up_to_ten)
