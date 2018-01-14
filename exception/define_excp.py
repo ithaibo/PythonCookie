ab4feac4a899bad5474ee7185e2bab49b962b44b
Andy <ithaibo@sina.com>
2018-01-14 21:17:40 +0800

    modify

--- exception/define_excp.py
+++ exception/define_excp.py
@@ -2,6 +2,9 @@ class Error(Exception):
     """Base class for exception in this modules"""
     pass
 
+    def showCaused():
+        return "something error happen"
+
 try:
     raise Error
 except Error:
