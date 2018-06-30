59ba75b961322e192e2cd2fefcedfbf4ddc3eb84
AndyWu <ithaibo@sina.com>
2018-06-30 14:51:20 +0800

    [dev]:{具有一些清理功能的基本服务器}

--- net/chat/server/ChatServer.py
+++ net/chat/server/ChatServer.py
@@ -1,13 +1,23 @@
 from asyncore import dispatcher
 import socket, asyncore
 
+PORT = 5000
+
 class ChatServer(dispatcher):
+
+    # 参数为端口
+    def __init__(self, port):
+        dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(('', port))
+        self.listen(5)
+
     def handle_accept(self):
         conn, addr = self.accept()
         print('Connection attempt from', addr[0])
 
-s = ChatServer()
-s.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 5000))
-s.listen(5)
-asyncore.loop()
+if __name__ == '__main__':
+    s = ChatServer(PORT)
+    try: asyncore.loop()
+    except KeyboardIntercept: pass
