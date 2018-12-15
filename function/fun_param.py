7e3bd7ed375a24efb5b073faab40ce08d3d8c5cd
Andy <hibowu30@gmail.com>
2018-12-15 23:01:08 +0800

    [dev]:{functions}

--- function/fun_param.py
+++ function/fun_param.py
@@ -1,3 +1,18 @@
 # function as paramters
-def apply_to_one(fun)
+def apply_to_one(fun):
     return fun(1)
+
+def print_dict(**params):
+    print params
+print_dict(x=1,y=2,z=3)
+
+def print_tuple(*params):
+    print params
+print_tuple(1, 2, 3)
+
+def print_1(x, y, z=3, *tp, **dt):
+    print x, y, z
+    print tp
+    print dt
+print_1(1,2,3, 4, 5, 6, foo=8, bar=9)
+print_1(1, 2)
